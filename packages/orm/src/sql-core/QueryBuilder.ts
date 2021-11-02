64b6117795b0a4fea110b9fb1dbf5ae5dada6098
Leonardo Avendaño <leo.aven94@gmail.com>
2021-11-01 22:56:04 -0300

    WIP

--- packages/orm/src/sql-core/QueryBuilder.ts
+++ packages/orm/src/sql-core/QueryBuilder.ts
@@ -1,72 +1,114 @@
 type QueryManipulation = 'SELECT' | 'INSERT' | 'UPDATE' | 'DELETE';
+type Direction = 'ASC' | 'DESC';
+type Operator =
+  | '='
+  | '!='
+  | '<'
+  | '<='
+  | '>'
+  | '>='
+  | 'LIKE'
+  | 'NOT LIKE'
+  | 'IN'
+  | 'NOT IN';
+
 interface Query {
-  manipulation: QueryManipulation;
+  type: QueryManipulation;
+  query: SelectQuery | InsertQuery | UpdateQuery | DeleteQuery;
+}
+interface CommonQuery {
   table: string;
-  columns?: string[];
-  where?: Where;
+  columns: string[];
+}
+interface SelectQuery extends CommonQuery {
+  where: Where;
   joins?: Join[];
-  returning?: string[];
-  values?: any[];
-  orderBy?: OrderBy;
+  orderBy?: OrderBy[];
   limit?: number;
   offset?: number;
   groupBy?: string[];
-  having?: Where;
+  //having?: Where;
 }
 
-export default class QueryBuilder {
-  private queryStratergy: QueryStratergy;
-  private builder: any;
+interface InsertQuery extends CommonQuery {
+  values: any[];
+}
 
-  select() {
-    return 0;
+interface UpdateQuery extends CommonQuery {
+  values: any[];
+  where?: Where;
+}
+
+interface DeleteQuery {
+  table: string;
+  where: Where;
+}
+
+interface Join {
+  table: string;
+  type: 'INNER' | 'LEFT' | 'RIGHT' | 'FULL';
+  on: Where;
+}
+
+interface OrderBy {
+  column: string;
+  direction: Direction;
+}
+
+interface Where {
+  column: string;
+  operator: Operator;
+  value: string | string[] | number | number[] | boolean | Date;
+}
+
+interface Between {
+  operator: 'BETWEEN';
+  from: string | number;
+  to: string | number;
+}
+
+interface Manipulator {
+  query: Query;
+  select(table: string, columns: string[]): Conditions;
+  insert(table: string, columns: string[], values: any[]): Conditions;
+  update(
+    table: string,
+    columns: string[],
+    values: any[],
+    where: Where,
+  ): Conditions;
+  delete(table: string, where: Where): Conditions;
+}
+
+interface Conditions {
+  query: Query;
+  join(table: string, on: Where): Conditions;
+  orderBy(column: string, direction: Direction): Conditions;
+  limit(limit: number): Conditions;
+  offset(offset: number): Conditions;
+  where(where: Where): Conditions;
+  groupBy(columns: string[]): Conditions;
+  having(where: Where): Conditions;
+  build(): string;
+}
+
+export class ManipulationBuilder implements Manipulator {
+  private query!: Query;
+  select(table: string, columns: string[]): Conditions {
+    throw new Error('Method not implemented.');
   }
-  insert() {
-    return 0;
+  insert(table: string, columns: string[], values: any[]): Conditions {
+    throw new Error('Method not implemented.');
   }
-  update() {
-    return 0;
+  update(
+    table: string,
+    columns: string[],
+    values: any[],
+    where: Where,
+  ): Conditions {
+    throw new Error('Method not implemented.');
   }
-  delete() {
-    return 0;
-  }
-  where() {
-    return 0;
-  }
-  innerJoin() {
-    return 0;
-  }
-  leftJoin() {
-    return 0;
-  }
-  rightJoin() {
-    return 0;
-  }
-  fullJoin() {
-    return 0;
-  }
-  union() {
-    return 0;
-  }
-  except() {
-    return 0;
-  }
-  intersect() {
-    return 0;
-  }
-  groupBy() {
-    return 0;
-  }
-  having() {
-    return 0;
-  }
-  orderBy() {
-    return 0;
-  }
-  limit() {
-    return 0;
-  }
-  offset() {
-    return 0;
+  delete(table: string, where: Where): Conditions {
+    throw new Error('Method not implemented.');
   }
 }
